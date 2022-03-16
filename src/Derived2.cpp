2b60aa7cdee3df05fd6929b42723f7f6f659a803
LewMontes <101229716+LewMontes@users.noreply.github.com>
2022-03-15 19:54:56 -0600

    Added methods & attributes
    Added corresponding methods and attributes to struct Derived2

--- src/Derived2.cpp
+++ src/Derived2.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Derived2.h"
+
+Derived2::Derived2(int baseNum, int derivedNum2) : Base(baseNum), derivedNum2(derivedNum2) {}
+
+int Derived2::getDerivedNum2() const {
+    return derivedNum2;
+}
+
+void Derived2::setDerivedNum2(int derivedNum2) {
+    Derived2::derivedNum2 = derivedNum2;
+}
+
+Derived2::~Derived2() {
+
+}
+
+string Derived2::toString() {
+    stringstream s;
+
+    s << "Derived Class Type 2 - Num. " << getDerivedNum2() << endl;
+
+    return s.str();
+}
+
+void Derived2::doSomething() {
+    cout << "Derived2!!!" << endl;
+}
--- src/Derived2.h
+++ src/Derived2.h
@@ -5,9 +5,24 @@
 #ifndef LAB01_INHERITANCE_DERIVED2_H
 #define LAB01_INHERITANCE_DERIVED2_H
 
+#include "Base.h"
 
-class Derived2 {
+struct Derived2 : Base {
+private:
+    int derivedNum2;
 
+public:
+    Derived2(int baseNum, int derivedNum2);
+
+    int getDerivedNum2() const;
+
+    void setDerivedNum2(int derivedNum2);
+
+    virtual ~Derived2();
+
+    string toString() override;
+
+    virtual void doSomething();
 };
 
 
